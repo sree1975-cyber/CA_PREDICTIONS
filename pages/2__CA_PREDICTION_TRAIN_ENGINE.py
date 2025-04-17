71e2e51a71fc05ba30cd1634618541ef0f707ded
sree1975-cyber <sreeduba@hotmail.com>
2025-04-17 14:24:46 -0400

    Update 2__CA_PREDICTION_TRAIN_ENGINE.py

--- pages/2__CA_PREDICTION_TRAIN_ENGINE.py
+++ pages/2__CA_PREDICTION_TRAIN_ENGINE.py
@@ -737,8 +737,7 @@ elif app_mode == "Single Student Check":
                             value=academic_performance
                         )
                     
-                      if st.button("Run Scenario Analysis"):
-                    
+                    if st.button("Run Scenario Analysis"):
                         changes = {
                             'Present_Days': new_attendance,
                             'Absent_Days': (present_days + absent_days) - new_attendance,
